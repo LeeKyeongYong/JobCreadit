228f207e1c6f5c027683a5e4e1f60f602029a14d
이경용 <sleekydz86@icloud.com>
2024-07-09 21:56:26 +0900

    프로젝트 셋팅

---
+++ src/test/kotlin/org/jc/jobcreadit/JobCreaditApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jc.jobcreadit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobCreaditApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
